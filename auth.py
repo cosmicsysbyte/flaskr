d5d7048e84f3f25eb58ab3a27dfac69ccd344b7b
Shamim Osman <cosmicsysbyte@gmail.com>
2024-01-19 21:25:37 +0600

    test app2

--- auth.py
+++ auth.py
@@ -3,9 +3,8 @@ import functools
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
-from werkzeug.security import check_password_hash, generate_password_hash
 
-from flaskapp.db import get_db
+from flaskr.db import get_db
 
 bp = Blueprint('auth', __name__)
 
