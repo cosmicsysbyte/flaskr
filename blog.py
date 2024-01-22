7eb6e54c6e177e104a9e68000f7a1f88635ed01f
Shamim Osman <cosmicsysbyte@gmail.com>
2024-01-22 16:37:49 +0600

    table add and many fixes

--- blog.py
+++ blog.py
@@ -14,9 +14,52 @@ def index():
     
     return render_template('blog/index.html')
 
-@bp.route('/addlink')
+@bp.route('/addlink', methods=('GET', 'POST'))
 @login_required
 def addlink():
-    db = get_db()
-    
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'INSERT INTO post (title, body, author_id)'
+                ' VALUES (?, ?, ?)',
+                (title, body, g.user['id'])
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+
     return render_template('blog/addlink.html')
+
+@bp.route('/addevent', methods=('GET', 'POST'))
+@login_required
+def addevent():
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'INSERT INTO post (title, body, author_id)'
+                ' VALUES (?, ?, ?)',
+                (title, body, g.user['id'])
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+
+    return render_template('blog/addevent.html')
